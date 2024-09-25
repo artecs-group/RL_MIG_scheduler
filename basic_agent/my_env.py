a4ef1a8cb62589fb8e83ff8880d200870accd5ba
Jorge <jorvil01@ucm.es>
2024-09-25 20:00:39 +0200

    Step method implemented (not tested enough yet) and also observation render

--- basic_agent/my_env.py
+++ basic_agent/my_env.py
@@ -1,9 +1,10 @@
+from collections import OrderedDict
 import gymnasium as gym
 import ray
 import numpy as np
 from ray.rllib.algorithms import ppo
-from gymnasium.spaces import Dict, Discrete, MultiDiscrete, Box
-from utils import partition_map, canonical_sort_tasks, print_obs
+from gymnasium.spaces import Dict, Discrete, MultiDiscrete, MultiBinary
+from utils import partition_map, instance_size_map, canonical_sort_tasks, print_obs
 
 
 
@@ -11,19 +12,28 @@ class SchedEnv(gym.Env):
     def __init__(self, env_config):
         self.N = env_config["N"]
         self.M = env_config["M"]
+        self.reconfig_time = 1 # Tiempo de reconfiguración 1 unidad, pensar cómo modelar adecuadamente
         self.observation_space = Dict({
             "observations": Dict({
                 "partition": Discrete(19), # 19 particiones posibles
                 "slices_t": MultiDiscrete([(self.M + 1)] * 7), # Cuanto le queda (de 0 a M) a cada slice
                 "ready_tasks": MultiDiscrete([[(self.M + 1)] * 5 + [(self.N + 1)]] * self.N) # El tipo de tarea (tiempo que tarda con cada una de los 5 tamaños e instancia) de las hasta N tareas libres, y la cantidad de tareas que hay de ese tipo (componente 6)
             }),
-            "action_mask": Box(0, 1, shape=(1 + 19 + 7 * self.N,)) # Máscara de acciones válidas
+            "action_mask": MultiBinary(1 + 19 + 7 * self.N,) # Máscara de acciones válidas
         })
 
         self.action_space = Discrete(1 + 19 + 7 * self.N) # 1 accion esperar, 19 acciones de configuración, y 7*N acciones de asignar tarea
 
 
-    def _get_action_mask(self, current_partition, slices_t, ready_tasks):
+    def _get_action_mask(self):
+        current_partition = self.obs["observations"]["partition"]
+        slices_t = self.obs["observations"]["slices_t"]
+        ready_tasks = self.obs["observations"]["ready_tasks"]
+
+        # La acción de esperar sólo es válida si hay tareas en la GPU
+        wait = 1 if any(slice_t != 0 for slice_t in slices_t) else 0
+
+
         # Reconfiguraciones válidas
         reconfig_mask = [1] * 19
         # Prohibido reconfigurar a la partición actual
@@ -46,7 +56,7 @@ class SchedEnv(gym.Env):
                 break
         
         # Prohibido asignar la tarea ready a una instancia con indice superior a la cantidad de instancias de la partición actual
-        first_forbidden_instance = len(partition_map[current_partition]["instances"])
+        first_forbidden_instance = len(partition_map[current_partition]["sizes"])
         for task in range(self.N):
             for instance in range(first_forbidden_instance, 7):
                 select_ready_task[task * 7 + instance] = 0
@@ -57,41 +67,116 @@ class SchedEnv(gym.Env):
             if slices_t[first_instance_slice] > 0:
                 for task in range(self.N):
                     select_ready_task[task * 7 + instance_index] = 0
-            first_instance_slice += partition_map[current_partition]["instances"][instance_index]
+            first_instance_slice += partition_map[current_partition]["sizes"][instance_index]
 
-        return [1] + reconfig_mask + select_ready_task
+        return [wait] + reconfig_mask + select_ready_task
         
+
+
+    def _get_numpy_obs_state(self):
+        return OrderedDict([('action_mask', np.array(self.obs["action_mask"])),
+                            ('observations', OrderedDict([('partition', np.int64(self.obs["observations"]["partition"])),
+                                                          ('ready_tasks', np.array(self.obs["observations"]["ready_tasks"])),
+                                                          ('slices_t', np.array(self.obs["observations"]["slices_t"]))]))])
+
+
 
     def reset(self, seed = None, options = None):
 
         num_ready = self.N if np.random.rand() < 0.8 else np.random.randint(1, self.N)
-        self.pending_tasks = [sorted(np.random.randint(1, self.M + 1, size=5), reverse=True) for _ in range(num_ready)]
+        pending_tasks = [sorted(np.random.randint(1, self.M + 1, size=5), reverse=True) for _ in range(num_ready)]
         init_partition = 1 # Por ahora, consideramos que se empieza en la partición 1 (una sola instancia de tamaño 7 siempre)
         init_slice_t = [0] * 7 # Consideramos que todos los slices están libres al principio
-        ready_tasks_canonical = canonical_sort_tasks(self.M, self.pending_tasks[:self.N]) # Las siguientes N tareas pendientes, se ordenan canónicamente y colocando como 6ª componente la cantidad de veces que se repite
+        ready_tasks_canonical = canonical_sort_tasks(self.M, pending_tasks[:self.N]) # Las siguientes N tareas pendientes, se ordenan canónicamente y colocando como 6ª componente la cantidad de veces que se repite
         # Usamos el 0 para rellenar posiciones vacías en la representación de tareas ready
         init_ready_tasks = ready_tasks_canonical + [[0] * 6] * (self.N - len(ready_tasks_canonical)) # Rellenamos con arrays de 6 ceros hasta N
 
-        initial_obs = {
+        self.obs = {
             "observations":{
                 "partition": init_partition,
-                "slices_t": np.array(init_slice_t),
-                "ready_tasks": np.array(init_ready_tasks)
-            },
-            "action_mask": self._get_action_mask(init_partition, init_slice_t, init_ready_tasks)
+                "slices_t": init_slice_t,
+                "ready_tasks": ready_tasks_canonical
+            }
         }
+        self.obs["action_mask"] = self._get_action_mask()
 
-        print_obs(initial_obs)
-
-        return initial_obs, {}
+        
+        return self._get_numpy_obs_state(), {}
     
+
+    def render(self):
+        print_obs(self.obs)
+
+
+
+    def _is_terminated(self):
+        # Si todas las tareas están terminadas, y todo lo que hay en la GPU ha acabado, el episodio termina
+        for slice_t in self.obs["observations"]["slices_t"]:
+            if slice_t > 0:
+                return False
+
+        for ready_task in self.obs["observations"]["ready_tasks"]:
+            if ready_task[-1] > 0:
+                return False
+        
+        return True
+
     def step(self, action):
+        # Esperar
+        if action == 0:
+            pass
+        # Reconfigurar
+        elif action <= 19:
+            self.obs["observations"]["partition"] = action
+            # Para la reconfiguración introduzco una tarea ficticia en las instancias libres
+            for slice, time in enumerate(self.obs["observations"]["slices_t"]):
+                if time == 0:
+                    self.obs["observations"]["slices_t"][slice] = self.reconfig_time
+        # Asignar tarea
+        else:
+            task = (action - 20) // 7
+            instance = (action - 20) % 7
+            # Quitamos la tarea de las ready_tasks
+            self.obs["observations"]["ready_tasks"][task][-1] -= 1
+            # Si es la última de cierto tipo, quitamos el tipo de ready task
+            if self.obs["observations"]["ready_tasks"][task][-1] == 0:
+                self.obs["observations"]["ready_tasks"].pop(task)
+                # Añadimos un tipo de tarea vacío al final para mantener la dimensionalidad
+                self.obs["observations"]["ready_tasks"].append([0] * 6)
+            # Aumentamos el tiempo que tarda la tarea para el tamaño de la instancia en todos los slices de la instancia
+            instance_size = partition_map["sizes"][instance]
+            task_time = self.obs["observations"]["ready_tasks"][task][instance_size_map[instance_size]]    
+            for i, instance_slice in enumerate(partition_map["instances"]):
+                if instance_slice == instance:
+                    self.obs["observations"]["slices_t"][i] = task_time
+
+        # Transitamos al primer slice que se libere
+        min_slice_time = min(self.obs["observations"]["slices_t"])
+        self.obs["observations"]["slices_t"] = [slice_time - min_slice_time for slice_time in self.obs["observations"]["slices_t"]]
+
+        # Actualizamos la máscara de acciones para el nuevo estado
+        self.obs["action_mask"] = self._get_action_mask()
+
+        # Recompensamos con -tiempo transcurrido, para minimizar el makespan
+        reward = -min_slice_time
+        # Comprobamos si el episodio ha terminado
+        terminated = self._is_terminated()
+        
+        truncated, info = False, {}
+
+        return self._get_numpy_obs_state(), reward, terminated, truncated, info
+
+    def close():
         pass
 
 
 
 
 env_example = SchedEnv({"N": 5, "M": 3})
-#obs = env_example.observation_space.sample()
+print("Example")
+print(env_example.observation_space.sample())
 initial_obs, _ = env_example.reset(seed=0)
-#print(initial_obs)
+env_example.render()
+print("\n\nAfter waiting")
+env_example.step(1)
+env_example.render()
--- basic_agent/utils.py
+++ basic_agent/utils.py
@@ -2,26 +2,30 @@ from collections import Counter
 
 # Mapa de número de partición a sus instancias
 partition_map = {
-    1: {"slices": ["7"] * 7, "instances": [7]}, 
-    2: {"slices": ["4"] * 4 + ["3"] * 3, "instances": [4, 3]}, 
-    3: {"slices": ["4"] * 4 + ["2"] * 2 + ["1"], "instances": [4, 2, 1]}, 
-    4: {"slices": ["4"] * 4 + ["1"] * 3, "instances": [4, 1, 1, 1]}, 
-    5: {"slices": ["3"] * 4 + ["3"] * 3, "instances": [3, 3]}, 
-    6: {"slices": ["3"] * 4 + ["2"] * 2 + ["1"], "instances": [3, 2, 1]}, 
-    7: {"slices": ["3"] * 4 + ["1"] * 3, "instances": [3, 1, 1, 1]}, 
-    8: {"slices": ["2"] * 4 + ["3"] * 3, "instances": [2, 2, 3]}, 
-    9: {"slices": ["2"] * 6 + ["1"], "instances": [2, 2, 2, 1]}, 
-    10: {"slices": ["2"] * 4 + ["1"] * 3, "instances": [2, 2, 1, 1, 1]}, 
-    11: {"slices": ["2"] * 2 + ["1"] * 2 + ["3"] * 3, "instances": [2, 1, 1, 3]}, 
-    12: {"slices": ["2"] * 2 + ["1"] * 2 + ["2"] * 2 + ["1"], "instances": [2, 1, 1, 2, 1]}, 
-    13: {"slices": ["2"] * 2 + ["1"] * 5, "instances": [2, 1, 1, 1, 1, 1]}, 
-    14: {"slices": ["1"] * 2 + ["2"] * 2 + ["3"] * 3, "instances": [1, 1, 2, 3]}, 
-    15: {"slices": ["1"] * 2 + ["2"] * 4 + ["1"], "instances": [1, 1, 2, 2, 1]}, 
-    16: {"slices": ["1"] * 2 + ["2"] * 2 + ["1"] * 3, "instances": [1, 1, 2, 1, 1, 1]}, 
-    17: {"slices": ["1"] * 4 + ["3"] * 3, "instances": [1, 1, 1, 1, 3]}, 
-    18: {"slices": ["1"] * 4 + ["2"] * 2 + ["1"], "instances": [1, 1, 1, 1, 2, 1]}, 
-    19: {"slices": ["1"] * 7, "instances": [1, 1, 1, 1, 1, 1, 1]}, 
+    1: {"slices": ["7"] * 7, "sizes": [7], "instances" : [0,0,0,0,0,0,0]}, 
+    2: {"slices": ["4"] * 4 + ["3"] * 3, "sizes": [4, 3], "instances" : [0,0,0,0,1,1,1]}, 
+    3: {"slices": ["4"] * 4 + ["2"] * 2 + ["1"], "sizes": [4, 2, 1], "instances" : [0,0,0,0,1,1,2]}, 
+    4: {"slices": ["4"] * 4 + ["1"] * 3, "sizes": [4, 1, 1, 1], "instances" : [0,0,0,0,1,2,3]}, 
+    5: {"slices": ["3"] * 4 + ["3"] * 3, "sizes": [3, 3], "instances" : [0,0,0,0,1,1,1]}, 
+    6: {"slices": ["3"] * 4 + ["2"] * 2 + ["1"], "sizes": [3, 2, 1], "instances" : [0,0,0,0,1,1,2]}, 
+    7: {"slices": ["3"] * 4 + ["1"] * 3, "sizes": [3, 1, 1, 1], "instances" : [0,0,0,0,1,2,3]}, 
+    8: {"slices": ["2"] * 4 + ["3"] * 3, "sizes": [2, 2, 3], "instances" : [0,0,1,1,2,2,2]}, 
+    9: {"slices": ["2"] * 6 + ["1"], "sizes": [2, 2, 2, 1], "instances" : [0,0,1,1,2,2,3]}, 
+    10: {"slices": ["2"] * 4 + ["1"] * 3, "sizes": [2, 2, 1, 1, 1], "instances" : [0,0,1,1,2,3,4]}, 
+    11: {"slices": ["2"] * 2 + ["1"] * 2 + ["3"] * 3, "sizes": [2, 1, 1, 3], "instances" : [0,0,1,2,3,3,3]}, 
+    12: {"slices": ["2"] * 2 + ["1"] * 2 + ["2"] * 2 + ["1"], "sizes": [2, 1, 1, 2, 1], "instances" : [0,0,1,2,3,3,4]}, 
+    13: {"slices": ["2"] * 2 + ["1"] * 5, "sizes": [2, 1, 1, 1, 1, 1], "instances" : [0,0,1,2,3,4,5]}, 
+    14: {"slices": ["1"] * 2 + ["2"] * 2 + ["3"] * 3, "sizes": [1, 1, 2, 3], "instances" : [0,1,2,2,3,3,3]}, 
+    15: {"slices": ["1"] * 2 + ["2"] * 4 + ["1"], "sizes": [1, 1, 2, 2, 1], "instances" : [0,1,2,2,3,3,4]}, 
+    16: {"slices": ["1"] * 2 + ["2"] * 2 + ["1"] * 3, "sizes": [1, 1, 2, 1, 1, 1], "instances" : [0,1,2,2,3,4,5]}, 
+    17: {"slices": ["1"] * 4 + ["3"] * 3, "sizes": [1, 1, 1, 1, 3], "instances" : [0,1,2,3,4,4,4]}, 
+    18: {"slices": ["1"] * 4 + ["2"] * 2 + ["1"], "sizes": [1, 1, 1, 1, 2, 1], "instances" : [0,1,2,3,4,4,5]}, 
+    19: {"slices": ["1"] * 7, "sizes": [1, 1, 1, 1, 1, 1, 1], "instances" : [0,1,2,3,4,5,6]}, 
 }
+
+
+# Mapa de tamaño de instancia a posición de array en que se codifica
+instance_size_map = {1: 0, 2: 1, 3: 2, 4: 3, 7: 4}
 
 
 # Pasa a entero desde M-ário como representación
@@ -47,7 +51,7 @@ def print_obs(obs):
     action_mask = obs["action_mask"]
     print("-----------")
     print("State:")
-    print("\tPartition:", partition_map[state["partition"]]["instances"])
+    print("\tPartition:", partition_map[state["partition"]]["sizes"])
     print("\tSlices:", state["slices_t"])
     for task in state["ready_tasks"]:
         if task[-1] != 0:
